46814345306c670b8eeb182348e0857d4f073745
Sam Cain <samcaingb@gmail.com>
2023-08-09 21:01:04 +0100

    Basic training working and saving

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.optim as optim
 import math
+import os
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print(f"Using device {device}")
 
-fd = FluxDataset("./data/samples/liver_10k.csv")
+folder_name = input("Please enter save name:")
+if not os.path.exists(os.path.join(folder_name)):
+    os.makedirs(os.path.join(folder_name))
+
+fd = FluxDataset(f"./data/samples/{folder_name}.csv")
 dl = DataLoader(fd, batch_size=64, shuffle=True);
 
-epochs = 50
+epochs = 100
+save_num = 10
 n_in = int(fd.data.shape[1])
 n_emb = 256
 n_lay = 5
@@ -45,3 +51,11 @@ for epoch in range(epochs):
             optimizer.step()
 
             t.set_description(f"Epoch [{epoch:3}] loss={loss:.4F}")
+
+    if epoch % save_num == 0:
+        torch.save(vae.encoder, os.path.join(folder_name, f"encoder{epoch}.pth"))
+        torch.save(vae.decoder, os.path.join(folder_name, f"decoder{epoch}.pth"))
+
+with open(os.path.join(folder_name, "losses.csv"), 'w') as file:
+    file.write("loss\n")
+    file.writelines(losses)
