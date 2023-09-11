f2ff51d305a2253fc82ee3dd6e30ec5a8e1d6f62
Sam <samciangb@gmail.com>
2023-09-11 10:51:57 +0100

    Added PCA and scatter to plotting.py

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 from plottingDataset import PlottingDataset
+from sklearn.decomposition import PCA
 from tqdm import tqdm
 
 import argparse
@@ -8,15 +9,19 @@ plt.rcParams['figure.dpi'] = 60
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("type", type=str, choices=["hist"])
+    parser.add_argument("type", type=str, choices=["hist", "scatter"])
+    parser.add_argument("-a", "--axis", type=int, nargs=2, default=[0,1])
+    parser.add_argument("--pca", action="store_true")
     parser.add_argument("datasets", nargs="+", type=str)
 
     return parser.parse_args()
 
-def hist(plotD):
+def expanded_datasets(args):
+    return args.datasets
+
+def hist(plotD, values):
     n = 8
     fig, axs = plt.subplots(n, n)
-    normalized_values = plotD.normalized_values()
 
     zeros = plotD.values() == 0
     possible_plots = [i for i in range(plotD.shape()[1] - 1) if not all(zeros[:,i])]
@@ -24,7 +29,7 @@ def hist(plotD):
 
     for i in range(0, n*n):
         ax = axs[i%n, int(i/n)]
-        ax.hist(normalized_values[:,possible_plots[i]], bins=40)
+        ax.hist(values[:,possible_plots[i]], bins=40, stacked=True, label=plotD.labels())
 
         title = plotD.columns()[i]
         if len(title) > 30:
@@ -38,11 +43,32 @@ def hist(plotD):
     fig.set_figwidth(100)
     plt.show()
 
+def scatter(plotD, values, args):
+    x_ax, y_ax = args.axis
+    x = values[:, x_ax]
+    y = values[:, y_ax]
+
+    plt.scatter(x, y, c=plotD.colors())
+    plt.legend(handles=plotD.handles())
+    plt.xlabel(x_ax)
+    plt.ylabel(y_ax)
+    plt.show()
+
 args = get_args()
 plotD = PlottingDataset()
-for path in tqdm(args.datasets, desc="Loading datasets"):
+for path in tqdm(expanded_datasets(args), desc="Loading datasets"):
     plotD.add_section(path)
+
+values = plotD.normalized_values()
+
+if args.pca == True:
+    print("Fitting PCA...")
+    pca = PCA()
+    pca.fit(values)
+    values = pca.transform(values)
 
 match args.type:
     case "hist":
-        hist(plotD)
+        hist(plotD, values)
+    case "scatter":
+        scatter(plotD, values, args)
--- plottingDataset.py
+++ plottingDataset.py
@@ -1,17 +1,8 @@
 import pandas as pd
+from matplotlib import cm
+import matplotlib.patches as patches
 import re
 import os
-
-#def prepare_section(df : pd.DataFrame) -> pd.DataFrame:
-#    df.drop(columns=df.columns[0:1], inplace=True)
-#
-#    all_zeros = (df == 0).all()
-#    zero_columns = [n for n in df.columns if all_zeros[n]]
-#    mean = df.mean()
-#    std = df.std()
-#    std[zero_columns] = 1.0
-#
-#    return (df - mean) / std, mean, std
 
 def prepare_section(df : pd.DataFrame) -> pd.DataFrame:
     df.drop(columns=df.columns[0:1], inplace=True)
@@ -29,7 +20,11 @@ class PlottingDataset:
     def __len__(self):
         return len(self.df)
 
-    def add_section(self, path):
+    def add_section(self, path : str):
+        if not path.endswith('.csv'):
+            [self.add_section(os.path.join(path, f)) for f in os.listdir(path) if f.endswith('.csv')]
+            return
+
         label = get_label_from_path(path)
 
         df = prepare_section(pd.read_csv(path))
@@ -42,6 +37,27 @@ class PlottingDataset:
 
     def values(self):
         return self.df.drop(columns=['label']).values
+    
+    def labels(self):
+        return self.df['label'].values
+    
+    def colors(self):
+        labels = self.df['label'].values
+        cl_dict = self.get_color_label_dict()
+        cols = [cl_dict[l] for l in labels]
+        return cols
+
+    def get_color_label_dict(self):
+        labels = self.df['label']
+        unique_labels = list(labels.unique())
+        unique_cols = list(cm.get_cmap('YlOrBr', len(unique_labels))._segmentdata)
+        return {l:col for l, col in zip(unique_labels, unique_cols)}
+        
+
+    def handles(self):
+        cl_dict = self.get_color_label_dict()
+        hadles = [patches.Patch(color=cl_dict[l], label=l) for l in cl_dict.keys()]
+        return hadles
     
     def normalized_values(self):
         df = self.df.drop(columns=['label'])
