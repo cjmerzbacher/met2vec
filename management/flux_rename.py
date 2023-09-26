b63a820ad0bbd44608eedfd73de752b2fab8e66c
Sam <samciangb@gmail.com>
2023-09-26 14:22:15 +0100

    Updated flux_raname to simplify

--- management/flux_rename.py
+++ management/flux_rename.py
@@ -28,15 +28,25 @@ def get_reaction_name(reaction):
 
     return name.replace(",", ".")
 
+def get_raname_dict(model):
+    return {r.id : get_reaction_name(r) for r in model.reactions}
 
-args = read_arguments()
-model = read_sbml_model(args.model)
-rename_dict = {r.id : get_reaction_name(r) for r in model.reactions}
+def rename_flux_df(flux_df, model):
+    rename_dict = get_reaction_name(model)
+    return flux_df.rename(columns=rename_dict)
 
-flux_df = pd.read_csv(args.edit).rename(columns=rename_dict)
 
-output_dir = os.path.dirname(args.output)
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
+def main():
+    args = read_arguments()
+    model = read_sbml_model(args.model)
+    flux_df = pd.read_csv(args.edit)
+    flux_df = rename_flux_df(flux_df, model)
 
-flux_df.to_csv(args.output, index=False)
+    output_dir = os.path.dirname(args.output)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    flux_df.to_csv(args.output, index=False)
+
+if __name__ == '__main__':
+    main()
