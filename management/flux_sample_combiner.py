b541a38267e3620d9559e70115032c73e0d91b72
Sam <samciangb@gmail.com>
2023-09-28 16:21:34 +0100

    Updated column management to work over multuple files

--- management/flux_sample_combiner.py
+++ management/flux_sample_combiner.py
@@ -18,7 +18,7 @@ def get_args():
     parser = argparse.ArgumentParser('Flux Sample Combiner', 'Used to combine multiple flux samples together to a larger sample.')
     parser.add_argument('action', choices=[UNION, INTER], help=f'Control whether the resulting combined sample keeps all columns ({UNION}) or only uses the columns which all samples have non-zero values ({INTER}).')
     parser.add_argument('input', help='The input directory from which all csv files will be parsed.')
-    parser.add_argument('output', help='The output file.')
+    parser.add_argument('output', help='The output directory where amended copies of all csv files will be saved.')
     parser.add_argument('--model', '-m', help='The location where genome scale models are stored in xml format.')
 
     return parser.parse_args()
@@ -37,21 +37,13 @@ def get_reaction_name(reaction):
     name = ''
     metabolites = reaction.metabolites
     for metabolite in sorted([m for m in metabolites], key=lambda m: m.name):
-        name += f'{metabolite.name}[{metabolite.compartment}][{metabolites[metabolite]}]'
+        name += f'{metabolite.name}({metabolite.compartment})[{metabolites[metabolite]}]'
 
     return name.replace(",", ".")
 
 def get_rename_dict(model):
     rd = {r.id : get_reaction_name(r) for r in model.reactions}
     return rd
-
-def update_sample_column_names(sample, model, inplace=False):
-    columns_rename_dict = get_rename_dict(model)
-    return sample.rename(columns=columns_rename_dict, inplace=inplace)
-
-def get_zero_columns(df : pd.DataFrame) -> list[str]:
-    all_zeros = np.all(df.values == 0, axis=0)
-    return df.columns[all_zeros]
 
 def read_sample(file):
     df = pd.read_csv(file)
@@ -64,42 +56,45 @@ def c(prompt = 'Continue (y/n)?'):
         user_input = input(prompt).lower()
         if user_input == 'n': quit()
 
-def rename_samples_columns(args, sample_files, samples):
-    model_files = [find_model_file(f, args) for f in sample_files]
-
-    print('Sample -> Model mapping')
-    for sf, mf, in zip(sample_files, model_files):
-        print(f'    {sf} -> {mf}')
+def check_model_folders(sample_files, args):
+    if args.model is None:
+        return
+    model_files = [find_model_file(f) for f in sample_files]
+    print('Sample -> Model mapping', *[f'  {sf} -> {mf}\n' for sf, mf in zip(sample_files, model_files)])
     c()
-
-    models = [read_sbml_model(f) for f in tqdm(model_files, 'Loading models')]
-    for sample, model in zip(samples, models):
-        update_sample_column_names(sample, model, inplace=True)
 
 def main():
     args = get_args()
     sample_files = find_files_with_extension(args, '.csv')
-    
-    samples = [read_sample(f) for f in tqdm(sample_files, 'Loading samples')]
+    check_model_folders(sample_files, args)
 
-    if args.model is not None:
-        rename_samples_columns(args, sample_files, samples)
+    column_sets = []
+
+    for sample_file in tqdm(sample_files, 'Finding columns to keep'):
+        sample = read_sample(sample_file)
+        start_n_columns = len(sample.columns)
         
-    for sample in samples:
-        zero_columns = set(get_zero_columns(sample))
+        if args.model is not None:
+            model = read_sbml_model(find_model_file(sample_file))
+            sample.rename(columns=get_rename_dict(model), inplace=True)
+            if start_n_columns != len(set(sample.columns)):
+                print("Err: nColumns has been reduced by model rename...")
+                quit()
+
+        zero_columns = sample.columns[np.all(sample.values == 0, axis=0)]
         sample.drop(columns=zero_columns, inplace=True)
 
-    combined_df = pd.concat(samples, join = 'inner' if args.action == INTER else 'outer')
-    combined_df.fillna(0, inplace=True)
+        column_sets.append(set(sample.columns))
 
-    output_dir = os.path.dirname(args.output)
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    combined_df.to_csv(args.output)
+    column_set = set().union(*column_sets) if args.action == UNION else set(column_sets[0]).intersection(*column_sets)
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
 
-     
-
-
+    for sample_file in tqdm(sample_files, 'Updating files'):
+        sample = read_sample(sample_file)
+        drop_columns = [c for c in sample.columns if c not in column_set]
+        sample.drop(columns=drop_columns, inplace=True)
+        sample.to_csv(os.path.join(args.output, os.path.basename(sample_file)))
 
 if __name__ == '__main__':
     main()
