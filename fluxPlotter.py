558686b5346ddfaffa72250fc8a0920adab77516
Sam <samciangb@gmail.com>
2023-10-25 12:13:11 +0100

    Updated dbscan mins type

--- fluxPlotter.py
+++ fluxPlotter.py
@@ -30,7 +30,7 @@ def get_args():
     parent_parser.add_argument('-t', '--title')
     parent_parser.add_argument('-s', '--save_plot')
     parent_parser.add_argument('--dbscan_eps', type=float, nargs=3, default=[1.0, 1.0, 1.0])
-    parent_parser.add_argument('--dbscan_mins', type=float, nargs=3, default=[5, 5, 5])
+    parent_parser.add_argument('--dbscan_mins', type=int, nargs=3, default=[5, 5, 5])
 
     # main parser
     parser = argparse.ArgumentParser('Flux Plotter')
