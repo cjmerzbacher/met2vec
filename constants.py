ce0ae9b6cadd4a97d8288a0969fc5ae8731727ed
Sam <samciangb@gmail.com>
2023-11-22 18:37:27 +0000

    Moved constats to constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,17 @@
-
+# Plotting
 PLOT_CONFIG_PATH = '.plotconfig.json'
 LABEL_CONFIG = 'lable_config'
 
+# VAE Stages
 PRE = 'pre'
 EMB = 'emb'
 REC = 'rec'
 VAE_STAGES = [PRE, EMB, REC]
+
+# Flux Dataset
+GEM_PATH_FOLDER = 'gems'
+RENAME_DICT_FILE = ".renaming.json"
+JOIN_FILE = ".join.json"
+PKL_FOLDER = ".pkl"
+DEFAULT_DATASET_SIZE = 65536
+DEFAULT_TEST_SIZE = 2048
