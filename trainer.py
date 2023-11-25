419c6d7aa9337b0872304375fc580ea8fc2b90d3
Sam <samciangb@gmail.com>
2023-11-25 14:14:33 +0000

    Updated trainer to use boolean_string

--- trainer.py
+++ trainer.py
@@ -6,6 +6,7 @@ import json
 from vae import VAE
 from vaeTrainer import VAETrainer
 from torch.utils.data import DataLoader
+from misc.parsing import boolean_string
 from fluxDataset import FluxDataset
 
 # Get arguments
@@ -17,15 +18,15 @@ parser.add_argument("--n_emb", default=128, type=int, help="The number of embedi
 parser.add_argument("--n_lay", default=5, type=int, help="The number of layers.")
 parser.add_argument("--lr", default=0.0001, type=float, help="The step size / learning rate used in SGD.")
 parser.add_argument("--lrelu_slope", type=float, default=0.0, help="The lrelu-slop used in the VAE.")
-parser.add_argument("--batch_norm", type=bool, default=False, help="Wether or not batch norm will be applied imbetween layers")
+parser.add_argument("--batch_norm", type=boolean_string, default=False, help="Wether or not batch norm will be applied imbetween layers")
 parser.add_argument("--dropout", default=0.0, type=float, help="The probability of a node being dropped out while training")
 parser.add_argument("-d" ,"--dataset", required=True, type=str, help="The directory or file the dataset is saved in.")
 parser.add_argument("-n", "--dataset_size", default=65536, type=int, help='The size of the dataset to be loaded for each epoch.')
 parser.add_argument("--join", choices=['inner', 'outer'], default='inner', help="How the different reaction sets should be joined.")
 parser.add_argument("-r", "--refresh_data_on", default=1, type=int, help="The number of epochs between changing the mix files (if used).")
 parser.add_argument("--save_losses_on", type=int, default=1, help="To reduce the number of losses saved, this allows evaluations which are a multiple to be saved.")
-parser.add_argument("--reload_dataset_aux", type=bool, default=False, help="Used to set reload_aux on the flux dataset.")
-parser.add_argument("--dataset_skip_tmp", type=bool, default=False, help="If true dataset is prevented reloading tmps.")
+parser.add_argument("--reload_dataset_aux", type=boolean_string, default=False, help="Used to set reload_aux on the flux dataset.")
+parser.add_argument("--dataset_skip_tmp", type=boolean_string, default=False, help="If true dataset is prevented reloading tmps.")
 parser.add_argument("--test_size", type=int, default=2048, help='The size of the test set.')
 parser.add_argument("main_folder", type=str, help="Name of the folder data will be saved to.")
 args = parser.parse_args()
