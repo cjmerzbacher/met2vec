08a716528055000f61244e06ae9f7c2b40ca7725
Sam <samciangb@gmail.com>
2024-01-04 12:32:15 +0000

    Change made for pressentation

--- test_fluxDataset_seed.py
+++
@@ -1,7 +0,0 @@
-from fluxDataset import *
-import numpy as np
-
-fd1 = FluxDataset("./data/samples/small_human", 300, 0, seed=0)
-fd2 = FluxDataset("./data/samples/small_human", 300, 0, seed=0)
-
-print(np.max(fd1.values - fd2.values))
