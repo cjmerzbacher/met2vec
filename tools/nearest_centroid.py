b7a310067410071a6c8dad4ec1ab0d384635c387
Samuel Cain <s2097950@ed.ac.uk>
2024-02-14 16:34:57 +0000

    New version of nc

--- tools/nearest_centroid.py
+++ tools/nearest_centroid.py
@@ -8,11 +8,11 @@ from misc.vae import load_VAE, get_load_VAE_args
 from misc.fluxDataset import load_fd, get_data, get_fluxes
 from misc.constants import *
 from misc.parsing import *
-from misc.classifier import *
 from tqdm import tqdm
 
+from sklearn.neighbors import NearestCentroid
+
 import numpy as np
-import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(parents=[
@@ -44,7 +44,26 @@ group_by = args.group_by
 train_df = get_data(train_fd, vae, stage, False, fluxes, source_columns=[group_by])
 test_df = get_data(test_fd, vae, stage, False, fluxes, source_columns=[group_by])
 
-pred = get_mean_pred(train_df, group_by)
+train_groups = sorted(map(str, train_df[group_by].unique()))
+test_groups = sorted(map(str, test_df[group_by].unique()))
 
-df = get_prediction_df(test_df, train_df, group_by, pred)
+train_df[group_by] = train_df[group_by].map(str).map(train_groups.index)
+
+train_X = train_df.drop(columns=group_by).values
+test_X = test_df.drop(columns=group_by).values
+
+train_y = train_df[group_by].values
+
+
+nc = NearestCentroid().fit(train_X, train_y)
+
+
+test_df[f"pred_{group_by}"] = np.array(train_groups)[nc.predict(test_X)]
+
+df = test_df.drop(columns=[c for c in test_df.columns if group_by not in c])
+for i, g in enumerate(train_groups):
+    df[f"{g}"] = df[f"pred_{group_by}"].map(lambda p: 1 if p == g else 0)
+
+df = df[train_groups + [group_by]].groupby(group_by).mean(numeric_only=True).reset_index()
+print(df)
 df.to_csv(args.save_path, index=False)
